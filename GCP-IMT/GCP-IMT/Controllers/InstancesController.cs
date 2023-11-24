8f428f77798d357381648a9203d2588b8abea27e
Mukesh K <92352730+Mukeshk1407@users.noreply.github.com>
2023-11-24 11:37:47 +0530

    Initial Commit
    Initial Commit

---
+++ GCP-IMT/GCP-IMT/Controllers/InstancesController.cs
@@ -0,0 +1,105 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Compute.v1;
+using Google.Cloud.Compute.V1;
+using Grpc.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GCP_IMT.Controllers
+{
+    [ApiController]
+    [Route("api/instancelist")]
+    public class InstancesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInstanceList()
+        {
+            try
+            {
+                // Load the service account key from the stored file
+                //var directoryPath = "D:/Key/Key1.json";
+                //var fileName = HttpContext.Session.GetString("ServiceAccountKeyName");
+                var keyFilePath = "D:\\Keys\\key1.json";
+                var jsonKey = System.IO.File.ReadAllText(keyFilePath);
+
+                // Deserialize the content into a class representing the expected structure
+                var serviceAccountKey = JsonConvert.DeserializeObject<ServiceAccountKey>(jsonKey);
+
+                if (serviceAccountKey == null)
+                {
+                    return BadRequest(new { Message = "Invalid service account key file format" });
+                }
+
+                // Access the project_id property
+                var projectId = serviceAccountKey.project_id;
+
+                // Authenticate with Google Cloud using the service account key
+                // Authenticate with Google Cloud using the service account key
+                var credential = GoogleCredential.FromJson(jsonKey).CreateScoped(ComputeService.Scope.CloudPlatform);
+
+                // Create a Compute Engine client
+                var instancesClient = new InstancesClientBuilder
+                {
+                    ChannelCredentials = credential.ToChannelCredentials()
+                }.Build();
+
+                // Fetch the list of instances in the desired project
+                var instances = ListInstances(instancesClient, projectId);
+
+                // Return the list of instances
+                return Ok(new { Message = "Instance list retrieved successfully", Instances = instances });
+            }
+            catch (Exception ex)
+            {
+                // Handle authentication or API call errors
+                return BadRequest(new { Message = $"Error retrieving instance list: {ex.Message}" });
+            }
+        }
+
+        private List<string> ListInstances(InstancesClient instancesClient, string projectId)
+        {
+            // Create a ListInstancesRequest
+            var listInstancesRequest = new ListInstancesRequest
+            {
+                Project = projectId,
+                Zone = "us-west4-b",
+            };
+
+            // List instances in the specified project and zone
+            var instanceList = instancesClient.List(listInstancesRequest);
+
+            // Extract instance names
+            var instances = new List<string>();
+            foreach (var page in instanceList.AsRawResponses())
+            {
+                // Extract instance names from the page
+                foreach (var instance in page.Items)
+                {
+                    instances.Add(instance.Name);
+                }
+            }
+
+            return instances;
+        }
+
+        // Define a class representing the expected structure of the service account key
+        private class ServiceAccountKey
+        {
+            // Define properties matching the structure of the service account key JSON
+            public string type { get; set; }
+            public string project_id { get; set; }
+            public string private_key_id { get; set; }
+            public string private_key { get; set; }
+            public string client_email { get; set; }
+            public string client_id { get; set; }
+            public string auth_uri { get; set; }
+            public string token_uri { get; set; }
+            public string auth_provider_x509_cert_url { get; set; }
+            public string client_x509_cert_url { get; set; }
+            public string universe_domain { get; set; }
+        }
+    }
+}
---
+++ GCP-IMT/GCP-IMT/Controllers/KeyUploadController.cs
@@ -0,0 +1,108 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Compute.V1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GCP_IMT.Controllers
+{
+    [ApiController]
+    [Route("api/uploadkey")]
+    public class KeyUploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult UploadKey(IFormFile keyFile)
+        {
+            try
+            {
+                // Ensure the directory exists, create it if not
+                var directoryPath = "D:/Keys";
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                // Save the file to the specified directory
+                var filePath = Path.Combine(directoryPath, "key1.json");
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    keyFile.CopyTo(stream);
+                }
+
+                // Validate the content of the uploaded file
+                var content = System.IO.File.ReadAllText(filePath);
+                var isValid = ValidateServiceAccountKey(content);
+
+                if (!isValid)
+                {
+                    // If validation fails, delete the file and return an error
+                    System.IO.File.Delete(filePath);
+                    return BadRequest(new { Message = "Invalid service account key file format" });
+                }
+
+                // Store the file name in session
+         //       HttpContext.Session.SetString("ServiceAccountKeyName", "key1.json");
+
+                return Ok(new { Message = "Service account key uploaded successfully" });
+            }
+            catch (Exception ex)
+            {
+                // Handle file upload or other errors
+                return BadRequest(new { Message = $"Error uploading service account key: {ex.Message}" });
+            }
+        }
+
+        private bool ValidateServiceAccountKey(string content)
+        {
+            try
+            {
+                // Deserialize the content into a class representing the expected structure
+                var serviceAccountKey = JsonConvert.DeserializeObject<ServiceAccountKey>(content);
+
+                // Perform necessary checks for required properties
+                if (string.IsNullOrEmpty(serviceAccountKey?.type) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.project_id) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.private_key_id) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.private_key) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.client_email) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.client_id) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.auth_uri) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.token_uri) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.auth_provider_x509_cert_url) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.client_x509_cert_url) ||
+                    string.IsNullOrEmpty(serviceAccountKey?.universe_domain))
+                {
+                    return false;
+                }
+
+                // Additional validation logic if needed
+
+                return true;
+            }
+            catch (JsonException)
+            {
+                return false; // JSON deserialization failed
+            }
+        }
+
+        // Define a class representing the expected structure of the service account key
+        private class ServiceAccountKey
+        {
+            // Define properties matching the structure of the service account key JSON
+            public string type { get; set; }
+            public string project_id { get; set; }
+            public string private_key_id { get; set; }
+            public string private_key { get; set; }
+            public string client_email { get; set; }
+            public string client_id { get; set; }
+            public string auth_uri { get; set; }
+            public string token_uri { get; set; }
+            public string auth_provider_x509_cert_url { get; set; }
+            public string client_x509_cert_url { get; set; }
+            public string universe_domain { get; set; }
+        }
+    }
+}
